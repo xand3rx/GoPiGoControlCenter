af615604c4ce2b817c1cbb37d83a404eb86d15b1
xander <xand3r@gmail.com>
2017-11-13 13:55:35 +0100

    initial project created

--- GoPiGoControlCenter.Tests/Controllers/HomeControllerTest.cs
+++ GoPiGoControlCenter.Tests/Controllers/HomeControllerTest.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GoPiGoControlCenter;
 using GoPiGoControlCenter.Controllers;
 
 namespace GoPiGoControlCenter.Tests.Controllers
